2885f371fb1ca60bf83a2dce3f10bfe854b135c8
unknown <dvpamarasinghe31@gmail.com>
2023-06-02 13:47:11 +0530

    Make the student's says cards

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
-$(function(){
-    $("#nav-placeholder").load('layouts/nav.html');
-  });
+var swiper = new Swiper(".mySwiper", {
+  pagination: {
+    el: ".swiper-pagination",
+
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  slidesPerView: 1,
+      slidesPerGroupSkip: 2,
+  breakpoints: {
+    769: {
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+    },
+  },
+  spaceBetween: 30,
+ 
+  loop:true,
+});
