2bf4e93de75022d4aa81171a47b7b63d56f6e7c2
unknown <dvpamarasinghe31@gmail.com>
2023-06-03 02:20:19 +0530

    Make changes to about

--- script.js
+++ script.js
@@ -19,3 +19,22 @@ var swiper = new Swiper(".mySwiper", {
  
   loop:true,
 });
+//purecounter
+new PureCounter();
+
+new PureCounter({
+
+  selector: '.purecounter',
+
+
+  start: 0,
+  duration: 4,
+  delay: 10,
+  once: true,
+  repeat: false,
+  decimals: 0,
+  legacy: true,
+  filesizing: false,
+  currency: false,
+  separator: false,
+});
