1bf94ef8f76acb0d60472ff0fc09bcf2bfbe87b5
unknown <dvpamarasinghe31@gmail.com>
2023-06-10 00:57:55 +0530

    add placeholders and make the contact page

--- script.js
+++ script.js
@@ -53,3 +53,11 @@ var swiper = new Swiper(".studentSwiper", {
   },
   loop:true,
 });
+
+
+$(function(){
+  $("#nav-placeholder").load("assets/placeholders/nav.html");
+});
+$(function(){
+  $("#footer-placeholder").load("assets/placeholders/footer.html");
+});
